241f9d7d84fa60bed31fbf153c9cef1f413e38c5
ciszkin <ciszka.v@gmail.com>
2020-02-11 11:16:16 +0300

    First commit (homework #14)

---
+++ src/Human.kt
@@ -0,0 +1,31 @@
+class Human {
+    var name: String? = null
+
+    class Student {
+        var studentID: Int? = null
+
+        fun sayID() = println("My ID is $studentID")
+    }
+
+    inner class Teacher() {
+        var homeWorkID: Int? = null
+
+        constructor(name: String?) : this() {
+            this@Human.name = name
+        }
+
+        fun sayHello() = println("My name is $name")
+
+        fun giveHomeWork() = println("Homework id is ${homeWorkID ?: 14}")
+    }
+}
+
+fun main() {
+    val stud = Human.Student()
+    //val teacher = Human().Teacher()
+    val teacher = Human().Teacher("Alex")
+
+    stud.sayID()
+    teacher.sayHello()
+    teacher.giveHomeWork()
+}
